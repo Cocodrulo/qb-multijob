515f91f9ce8d290a6f63a7e6ff71b56f0846b6ca
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2025-09-27 11:16:00 +0100

    Created fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Cocodrulo - QBCore"
+version "0.0.1"
+description "A multi-job script for QBCore framework"
+
+shared_scripts {
+	'@qb-core/shared/locale.lua',
+    "Config.lua",
+	'locales/*.lua',
+}
+
+client_scripts {
+    "client/main.lua"
+}
+
+server_scripts {
+    "server/main.lua"
+}
